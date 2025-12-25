b89fee911cd24f67b2f7afde9f4857d21b0a591a
Yash Munde <yashmunde68@gmail.com>
2025-12-25 11:17:52 +0530

    js output based questions

--- Frontend Concepts/frontend-practice.js
+++ Frontend Concepts/frontend-practice.js
@@ -1,1 +1,9 @@
 console.log("this file is about practicing the frontend concepts");
+
+// Switch statement
+const arr = [1, 2, 3, 4];
+// string concatenation concept
+
+let a = 10;
+let b = 20;
+console.log("The value of " + a + b);
