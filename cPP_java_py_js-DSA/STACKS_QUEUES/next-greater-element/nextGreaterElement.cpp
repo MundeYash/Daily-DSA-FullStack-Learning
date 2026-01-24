bffd438bdf86ddab4d84794b92460445081eee9e
Yash Munde <yashmunde68@gmail.com>
2026-01-25 00:13:24 +0530

    next-greater-element-optimised-stack-cpp

--- cPP_java_py_js-DSA/STACKS_QUEUES/next-greater-element/nextGreaterElement.cpp
+++ cPP_java_py_js-DSA/STACKS_QUEUES/next-greater-element/nextGreaterElement.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 /*NEXT-GREATER-ELEMENT - Given an array of integers and we need to find next greater element (element which is just greater than current value on right side of the array ) for each element of the array  */
 
+/*---------------------Optimsised Approach-------------------------------------------------------------------*/
+vector<int> nextGreaterElement_Approach2_Optimised(vector<int> arr)
+{
+    // concept : Stack based solution
+    // time : O(n), space :O(n)
+    // pending to solve
+}
+
+/*----------------------------------------------------------------------------------------*/
 // helper function for finding greater element on right
 int findNextGreaterElementOnRight(vector<int> arr, int index)
 {
@@ -37,7 +46,7 @@ int main()
 
     vector<int> arr = {6, 8, 0, 1, 3};
     printArray(arr);
-    vector<int>ans1 = nextGreaterElement_Approach1_Brute(arr); 
+    vector<int> ans1 = nextGreaterElement_Approach1_Brute(arr);
     printArray(ans1);
     return 0;
 }
