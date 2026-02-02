be9cda6ec68f6a1fccb1bdc67f38646beb80cd15
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2026-02-02 04:48:53 +0000

    pow of 2 implementation

---
+++ cPP_java_py_js-DSA/9Maths/checkpoweof2/checkPowOf2.js
@@ -0,0 +1,1 @@
+// write js code for finding whether given number is power of  2 or not 
---
+++ cPP_java_py_js-DSA/9Maths/checkpoweof2/checkPowof2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include "../../0commonFunctions/printComplexity.h"
+using namespace std;
+
+/*
+    Given a number and we need check whether it is power of 2 or not.
+*/
+
+int checkPowerofTwo_Approach_Brute(int n)
+{
+    printComplexities("O(31 * N)", "O(1)");
+    // integer range vary from (-2^31 <= n <= 2^31 - 1)
+    // concept : power function
+    // time : O(31 * n) , space :O(1)
+    for (int i = 0; i <= 31; i++)
+    {
+        if (pow(2, i) == n)
+            return true;
+    }
+    return false;
+}
+int checkPowerOfTwo_Approach_Optimised(int n)
+{
+
+    printComplexities("O(n)", "O(1)");
+    // concept : bits + check 0 bit
+    if (n == 0)
+        return false; // edge case
+    int ld;
+    while (n)
+    {
+        ld = (n & 1);
+        if (ld == 1)
+            break;
+
+        n = n >> 1;
+    }
+    // update the number and check
+    n = n >> 1;
+    if (n != 0)
+        return false;
+    return true;
+}
+int main()
+{
+    cout << checkPowerOfTwo_Approach_Optimised(2) << endl;
+    cout << checkPowerOfTwo_Approach_Optimised(6) << endl;
+    cout << checkPowerOfTwo_Approach_Optimised(8) << endl;
+    cout << checkPowerOfTwo_Approach_Optimised(12) << endl;
+    cout << "Check whether given number is pow of 2 or not " << endl;
+    return 0;
+}
