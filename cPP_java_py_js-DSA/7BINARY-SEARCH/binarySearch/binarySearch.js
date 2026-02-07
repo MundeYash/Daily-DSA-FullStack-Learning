48d0909762bd2635b72c113ddd942c6acc63f101
Yash Munde <yashmunde68@gmail.com>
2026-02-07 23:53:28 +0530

    Binary Search Implementation

--- cPP_java_py_js-DSA/7BINARY-SEARCH/binarySearch/binarySearch.js
+++ cPP_java_py_js-DSA/7BINARY-SEARCH/binarySearch/binarySearch.js
@@ -1,6 +1,22 @@
-// BINARY SEARCH CONCEPT: Given a sorted array and we need to find the whether the key exits in the array or not 
+import { printComplexityOfSolution } from "../../0commonFunctions/printComplexities.js";
 
+// BINARY SEARCH CONCEPT: Given a sorted array and we need to find the whether the key exits in the array or not
 
+let arr = [1, 2, 3, 4, 5, 5];
 
-let arr = [];
+function binarySearchImplement(arr, target) {
+  printComplexityOfSolution("binary search", "O(n log n ", "O(1)");
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
 
+    if (arr[mid] == target) return mid;
+    else if (arr[mid] > target) {
+      high = mid - 1;
+    } else low = mid + 1;
+  }
+  return -1;
+}
+
+console.log("index of target ", arr, binarySearchImplement(arr, 2));
