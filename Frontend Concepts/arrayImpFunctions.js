7922b85990de75a5ffa3ffe0b28b07ec2b29beb9
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2026-02-09 14:19:30 +0000

    callbacks , array concepts , DOM manipulation

--- Frontend Concepts/arrayImpFunctions.js
+++ Frontend Concepts/arrayImpFunctions.js
@@ -1,1 +1,40 @@
 // Important function(higher order functions in array )
+
+// forEach method (Higher-order-function ) : does not change original array , does not return anything like new array 
+
+let arr = [1, 2, 3, 4];
+let print = (a) => console.log(a);
+arr.forEach(print);
+
+
+// map method : modify the original array  , it return a new array 
+let newArray = arr.map((a)=> (2*a));
+console.log(newArray);
+console.log(arr ); // original array does not get change 
+
+
+// filter method : gives new array with filtered value 
+let filteredArray = arr.filter((item)=>(item &1 )); // will extract odd values 
+console.log("filtered value is ", filteredArray);
+
+// reduce method : it gives a single value 
+
+
+// find method 
+
+// includes method 
+
+
+// slice method : does not modify the original array 
+let slicedArray = arr.slice(1,4); // it give a subarray ( from start index to endIndex -1 )
+console.log("slice arr ",slicedArray);
+console.log(arr);
+
+// splice method : it modifies the original array 
+let splicedArray = arr.splice(1,4); 
+console.log("spliced array ", splicedArray); 
+console.log(arr);// here original array is modified 
+
+
+
+
