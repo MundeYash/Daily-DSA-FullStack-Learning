338b6525c63805fdbf24199524996e52bbedf10d
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2026-03-13 03:15:58 +0000

    code formatting and array intersection problem js

--- cPP_java_py_js-DSA/data-structure/hashmap/hashmap.js
+++ cPP_java_py_js-DSA/data-structure/hashmap/hashmap.js
@@ -11,6 +11,13 @@ console.log(mp);
 printHashMap(mp);
 
 //1. inserion operation : insert operation 
+/*
+    mp.set(key, value) : to set / insert some value 
+
+    mp.get(key): to acess the value of key present in the map 
+
+    mp.has(key): it check whether the key present in the map or not 
+*/
 mp[0]=1 ; 
 mp[1]=10;
 mp[20]=10;
