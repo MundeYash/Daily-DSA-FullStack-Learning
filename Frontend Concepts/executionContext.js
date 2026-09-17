5bb60821706a3b35b2372ec5d3b714e9bf3bcf51
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2026-09-17 02:41:35 +0000

    js concepts - code execution

---
+++ Frontend Concepts/executionContext.js
@@ -0,0 +1,8 @@
+// Execution context : It is the area where our code is getting executed
+// It contains two parts : memory component and code component
+
+// In memory components : All the variable are provided memory and intialized with undefined variables
+
+// Code component : here line by line code is getting executed and operations are performed
+
+// Call Stack: (Program Stacks)| (control stack)| (runtime stack)|(execution context stack)|(machine stack)| (runtime stack)
--- Frontend Concepts/variableHoisting.js
+++ Frontend Concepts/variableHoisting.js
@@ -1,3 +1,29 @@
+// Hoisting in javascript with real time examples
+
 // variable hoisting for var,let and const
 
 // scopes: local, global, functional , block scope
+getName();
+console.log(x);
+
+x = 100;
+
+function getName() {
+  x = 20;
+  console.log("My name is yash Munde");
+}
+getName();
+
+//
+
+var functionValue = getName;
+console.log(functionValue);
+functionValue();
+
+// code exectution step by step
+
+// function expression
+var getName2 = () => {
+  console.log("My name is yash Munde");
+};
+console.log(x);
